2e6b95976b2032d69ebf1d49e7a1ca18eadc1c09
StefanoND <sndobbin@gmail.com>
2025-03-18 10:20:27 +0000

    update

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -74,6 +74,19 @@ vim.opt.signcolumn = "yes" -- Show sign column so that text doesn't shift
 
 vim.opt.backspace = "indent,eol,start" -- Allow backspace on indent, end of line or insert mode start position
 
+-- OSC 52 (Operating System Command) support
+-- Control sequence that causes the terminal emulator to write to or read from the system clipboard.
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
 vim.opt.clipboard:append("unnamedplus") -- Use system clipboard as default register
 
 -- Split
