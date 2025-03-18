33308903a1d8db6858644d013256e4097fc9340c
StefanoND <sndobbin@gmail.com>
2025-03-18 11:54:07 +0000

    update

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -111,7 +111,7 @@ return {
 
       vim.keymap.set("n", "<C-t>", "<cmd>lua require'toggleterm'.exec('yazi')<CR>", opts)
 
-      vim.keymap.set("n", "<leader>ht", "<cmd>ToggleTerm<CR>", opts)
+      vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>", opts)
       vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
       vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
     end,
