7bba7fdc564c2cdb7f4f1c2ec77323c8774c467a
StefanoND <sndobbin@gmail.com>
2025-04-04 19:20:01 +0100

    update

--- filetype.lua
+++ filetype.lua
@@ -1,6 +1,5 @@
 -- Opt-in to use filetype.lua for setting custom filetypes
--- vim.g.do_filetype_lua = 1 -- Enable
-vim.api.nvim_set_var("do_filetype_lua", 1)
+vim.g.do_filetype_lua = 1 -- Enable
 
 vim.filetype.add({
   -- extension = {
---
+++ lua/ftplugin/bash.lua
@@ -0,0 +1,11 @@
+local ft = {}
+
+function ft.ftplugin()
+  vim.opt.expandtab = true
+  vim.opt.smartindent = true
+  vim.opt.tabstop = 4
+  vim.opt.softtabstop = 4
+  vim.opt.shiftwidth = 4
+end
+
+return ft
